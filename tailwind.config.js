5b45e81759fd5f4230485c923937a6e01eca73b6
choks <choks0225@naver.com>
2022-07-02 21:48:06 +0900

    chore: tailwind config 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,5 +11,8 @@ module.exports = {
       },
     },
   },
-  plugins: [require('tailwindcss-textshadow')],
+  plugins: [
+    require('tailwindcss-textshadow'),
+    require('tailwind-scrollbar-hide'),
+  ],
 };
